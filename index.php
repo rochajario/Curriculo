f92febc1a905dd29733869d6b6d718865af23d24
Jario Junior <56648231+rochajario@users.noreply.github.com>
2021-04-12 13:57:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Currículo Virtual</title>
-        <link href='style/style.css' rel='stylesheet'/>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-        <!--Scripts Para Uso de Todos Usuários-->
-        <script src='scripts/renderizador-boxes.js'></script>
-        <script src='scripts/usr-form-templates.js'></script>
-        <script src='scripts/usr-table-templates.js'></script>
-
-    </head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Currículo Virtual</title>
+    <link href='style/style.css' rel='stylesheet'/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <!--Scripts Para Uso de Todos Usuários-->
+    <script src='scripts/renderizador-boxes.js'></script>
+    <script src='scripts/usr-form-templates.js'></script>
+    <script src='scripts/usr-table-templates.js'></script>
 </head>
 <body class='main-theme'>
     <header class='main-page'>
